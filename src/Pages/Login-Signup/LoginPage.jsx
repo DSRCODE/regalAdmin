4778415a26ebb9b5869c7e708d01b1e9dd09e8fa
Mahendra Sen <mahendrasen249@gmail.com>
2024-01-20 08:23:40 +0530

    form validation

--- src/Pages/Login-Signup/LoginPage.jsx
+++ src/Pages/Login-Signup/LoginPage.jsx
@@ -4,25 +4,27 @@ import { Link } from "react-router-dom"
 import Lottie from "lottie-react";
 const LoginPage = () => {
     return (
-        <div className="login-box">
-            <div className="login-imagepart">
-                <Lottie className="lottie" animationData={SlideImage1} />
-            </div>
-            <form className="login-infopart">
-                <h3>Login</h3>
-                <label>Email ID</label>
-                <input type="text" name="" id="" placeholder="Enter Email ID" />
-                <label>Password</label>
-                <input type="password"  name="" id="" placeholder="Enter Password" />
-                <div className="loginbutton">
-                    <button type="submit" >Login</button>
+        <div className="login-container">
+            <div className="login-box">
+                <div className="login-imagepart">
+                    <Lottie className="lottie" animationData={SlideImage1} />
                 </div>
+                <form className="login-infopart">
+                    <h3>Login</h3>
+                    <label>Email ID</label>
+                    <input type="text" name="" id="" placeholder="Enter Email ID" />
+                    <label>Password</label>
+                    <input type="password" name="" id="" placeholder="Enter Password" />
+                    <div className="loginbutton">
+                        <button type="submit" >Login</button>
+                    </div>
 
-                <div className="forgotbox">
-                    <h6><Link to={"/forgot"} style={{ textDecoration: "none", color: "black", fontSize: "12px", letterSpacing: "1px" }}>Forgot Password?</Link></h6>
-                    <button className="signupbutton"><Link to={"/signup"} style={{ textDecoration: "none", color: "white" }}>Signup</Link></button>
-                </div>
-            </form>
+                    <div className="forgotbox">
+                        <h6><Link to={"/forgot"} style={{ textDecoration: "none", color: "black", fontSize: "12px", letterSpacing: "1px" }}>Forgot Password?</Link></h6>
+                        <button className="signupbutton"><Link to={"/signup"} style={{ textDecoration: "none", color: "white" }}>Signup</Link></button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
